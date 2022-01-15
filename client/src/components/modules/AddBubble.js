1db4f0b09364019d89bf29db78474b7ed5684b69
Amanda Mei <amandamei@Amandas-Macbook-Air.local>
2022-01-15 12:33:45 -0500

    recommit

--- client/src/components/modules/AddBubble.js
+++ client/src/components/modules/AddBubble.js
@@ -34,6 +34,9 @@ const AddBubble = (props) => {
 const StatusUpdate = (props) => {
     const updateStatus = (value) => {
         console.log(value);
+
+        /* const content = { content: props.status.content }
+        post("/api/status", content); /* TODO: make /api/status */ 
     };
 
     return <AddBubble defaultText="New Status Update" onSubmit={updateStatus}/>
