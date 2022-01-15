9ff783964a1ef8860f535a818ea4c6680984dcd2
Joli Dou <jolidou@dhcp-10-31-108-100.dyn.mit.edu>
2022-01-14 22:04:12 -0500

    Edit page D1

--- client/src/components/pages/Edit.js
+++ client/src/components/pages/Edit.js
@@ -1,29 +1,76 @@
 import React, { useState, useEffect } from "react";
+import "./Edit.css";
+import "../../utilities.css";
 
 const Edit = (props) => {
+  const [linkedIn, setLinkedin] = useState("");
+  const [insta, setInsta] = useState("");
+  const [fb, setFb] = useState("");
+  const [phone, setPhone] = useState(0);
 
-    return (
-        <>
-            <div>
-                {/* <div>{props.user.socialMedia}</div> */}
-                <div>{props.user.name}</div>
-                <div>
-                    <div onclick="location.href='/edit-profile'" type="button">Edit Profile</div>
+//   useEffect(() => {
+//     document.title = "Edit/Create Profile";
+//     get(`/api/user`, { userid: props.userId }).then((userObj) => setUser(userObj));
+//   }, []);
+
+//   if (!user) {
+//     return (<div> Loading! </div>);
+//   }
+  return (
+    <>
+        {/* <div className="Profile-avatar">
+            <h1> {user.name} </h1>
+         </div> */}
+        <body>
+        <div className = "wrapper" >
+            <div className = "columnContainer" >
+                <div className = "columnItem">
+                    <div className = "rowContainer">
+                        <div className = "rowItem">
+                            ACCOUNT INFO
+                            <div className = "rowContainer">
+                                    <span>
+                                        <input type = "text" name = "username" />
+                                        <input type = "text" name = "username" />
+                                    </span>
+                            </div>
+                        </div>
+                        <div className = "rowItem">
+                                <div className = "rowContainer">
+                                        <div className = "rowItem">
+                                            SOCIALS
+                                        </div>
+                                        <div className = "rowItem">
+                                            <input type = "text" name = "linkedIn" />
+                                        </div>
+                                        <div className = "rowItem">
+                                            <input type = "text" name = "fb" />
+                                        </div>
+                                        <div className = "rowItem">
+                                            <input type = "text" name = "instagram" />
+                                        </div>
+                                        <div className = "rowItem">
+                                            <input type = "text" name = "phone" />
+                                        </div>
+                                </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div className = "columnItem">
+                        <div className = "rowContainer">
+                            <div className = "rowItem">
+                                PROFILE PIC
+                            </div>
+                            <div className = "rowItem">
+                                <button>DONE EDITING</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div>
-                <div>Bubble Count:</div>
-                <div>{props.user.counter}</div>
-            </div>
-            <div>
-                <div>Contacts</div>
-                <span>{props.contact.friend}</span>
-                <span>{props.contact.romantic}</span>
-                <span>{props.contact.professional}</span>
-                <span>{props.contact.undecided}</span>
-            </div>
-        </>
-    )
-}
+        </body>
+    </>
+  );
+};
 
 export default Edit;
