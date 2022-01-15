88bb300a9535d506b3240a82dd1997ab7861f242
Joli Dou <jolidou@dhcp-10-31-108-100.dyn.mit.edu>
2022-01-15 00:11:58 -0500

    error page done

--- client/src/components/pages/NotFound.js
+++ client/src/components/pages/NotFound.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./NotFound.css";
+import "../../utilities.css";
 
-const NotFound = () => {
+const NotFound = (props) => {
   return (
-    <div>
-      <h1>404 Not Found</h1>
-      <p>The page you requested couldn't be found.</p>
-    </div>
+    <div class = "rainbow">
+            <p>Oops! We can't find the page you're looking for :(</p>
+            <div className = "pinkCircle" />
+            <div className = "turqCircle" />
+            <div className = "yellowCircle" />
+            <div className = "lightPinkCircle" />
+          </div>
   );
 };
 
