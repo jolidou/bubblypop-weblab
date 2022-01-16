79f890bb9fdacd97a5b224bf52f7643284f156ac
Joli Dou <jolidou@dhcp-10-31-108-100.dyn.mit.edu>
2022-01-16 17:54:07 -0500

    Created social card

---
+++ client/src/components/modules/SocialCard.js
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from "react";
+import { get } from "../../utilities";
+
+import BubbleCounter from "./BubbleCounter.js";
+import Status from "./Status.js";
+import Avatar from "./Avatar.js";
+import Contact from "./Contact.js";
+
+const socialCard  = (props) => {
+ HI
+}
+
+export default socialCard;
