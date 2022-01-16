173520903014eb5c0116723eca6ecef0285f262e
Joli Dou <jolidou@dhcp-10-31-108-100.dyn.mit.edu>
2022-01-16 14:22:54 -0500

    All

---
+++ client/src/components/modules/NavBar copy.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link } from "@reach/router";
+import GoogleLogin, { GoogleLogout } from "react-google-login";
+
+import "./NavBar.css";
+
+// This identifies your web application to Google's authentication service
+const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
+
+/**
+ * The navigation bar at the top of all pages. Takes no props.
+ */
+const NavBar = (props) => {
+  return (
+    <nav className="NavBar-container">
+      <div className="NavBar-title u-inlineBlock">Catbook</div>
+      <div className="NavBar-linkContainer u-inlineBlock">
+        <Link to="/" className="NavBar-link">
+          Home
+        </Link>
+        {props.userId && (
+          <Link to={`/profile/${props.userId}`} className="NavBar-link">
+            Profile
+          </Link>
+        )}
+        <Link to="/chat/" className="NavBar-link">
+          Chat
+        </Link>
+        {props.userId ? (
+          <GoogleLogout
+            clientId={GOOGLE_CLIENT_ID}
+            buttonText="Logout"
+            onLogoutSuccess={props.handleLogout}
+            onFailure={(err) => console.log(err)}
+            className="NavBar-link NavBar-login"
+          />
+        ) : (
+          <GoogleLogin
+            clientId={GOOGLE_CLIENT_ID}
+            buttonText="Login"
+            onSuccess={props.handleLogin}
+            onFailure={(err) => console.log(err)}
+            className="NavBar-link NavBar-login"
+          />
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
