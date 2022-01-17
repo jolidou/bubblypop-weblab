4ca2f2c556ba5d662222a7b9a59cddad8433bd6b
Sophie Zhang <sophiez@mit.edu>
2022-01-17 02:48:59 -0500

    delete tester schema

--- server/models/tester.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const TesterSchema = new mongoose.Schema({
-  user: String,
-  content: String,
-});
-
-// compile model from schema
-module.exports = mongoose.model("tester", TesterSchema);
