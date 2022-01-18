7a86806e42cbc3fa08fe09d6fc5fdb974fc706cc
Sophie Zhang <sophiez@mit.edu>
2022-01-17 21:45:44 -0500

    logout to login

--- client/src/components/pages/Logout.js
+++ client/src/components/pages/Logout.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import GoogleLogin from "react-google-login";
-import { Link } from "@reach/router";
+import { useNavigate } from "@reach/router";
 
 import "./Logout.css";
 import "../../utilities.css";
@@ -8,17 +8,20 @@ import "../../utilities.css";
 const GOOGLE_CLIENT_ID = "810319868270-ctj3rbt6ivtlc61roqv8top021nmccvj.apps.googleusercontent.com";
 
 const Logout = ({ handleLogin }) => {
+  const navigate = useNavigate();
   return (
     <>
-      <Link to={"/profile/"}>
-        <GoogleLogin
-          clientId={GOOGLE_CLIENT_ID}
-          buttonText="Login"
-          onSuccess={handleLogin}
-          onFailure={(err) => console.log(err)}
-          className="NavBar-link NavBar-login"
-        />
-      </Link>
+      <GoogleLogin
+        clientId={GOOGLE_CLIENT_ID}
+        buttonText="Login"
+        // onSuccess={handleLogin}
+        onSuccess={(res) => {
+          handleLogin(res);
+          navigate("/");
+        }}
+        onFailure={(err) => console.log(err)}
+        className="NavBar-link NavBar-login"
+      />
       {/* <div className="rainbow overlay">
         <h1 className="popMessage">You've been logged out! Please log in again to continue!</h1>
         <div className="circle pink" />
