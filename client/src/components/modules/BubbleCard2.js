514b500cd1e5dc616c29ee12a4eaba1c5c05a1f3
Joli Dou <jolidou@dhcp-10-31-108-100.dyn.mit.edu>
2022-01-17 22:05:41 -0500

    BubbleCard2

---
+++ client/src/components/modules/BubbleCard2.js
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from "react";
+import { get } from "../../utilities";
+
+import "./BubbleCard.css";
+import "../../utilities.css";
+
+const BubbleCard = (props) => {
+    const [avatar, setAvatar] = useState([]);
+    const [status, setStatus] = useState("");
+    
+    const updateStatus = (statusUpdate) => {
+        setStatus(statusUpdate);
+    };
+
+    useEffect(() => {
+        get("/api/status", { user: props.user, content: props.content }).then((statusObj) => {
+            setStatus(statusObj.content);
+        });
+        get("/api/avatar", { avatarURL: props.avatarURL }).then((statusObj => {
+            setAvatar(statusObj.avatar);
+        }))
+    }, []);
+
+    function hideDiv() {
+        document.getElementById("bubble").style.display="none";
+    }
+
+    return (
+        <div>
+            <div id = "bubble" className="bubbleContainer">
+                <div className = "bubbleContent">
+                    <p className = "bubbleUser"> {props.creator_id} </p>
+                    <div className = "bubbleStatus"> {props.content} </div>
+                    <button className = "bubbleButton" onClick= {hideDiv} > Pop!</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BubbleCard;
