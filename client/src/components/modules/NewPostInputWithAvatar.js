dacebcf6edb49d07adeef000bbe6d4663bf32154
Sophie Zhang <sophiez@mit.edu>
2022-01-21 23:56:49 -0500

    Attempt to create image upload button + submit string to Mongo (new files)

---
+++ client/src/components/modules/NewPostInputWithAvatar.js
@@ -0,0 +1,190 @@
+import React, { useState } from "react";
+
+import "./NewPostInput.css";
+import { post } from "../../utilities";
+
+/**
+ * New Post is a parent component for all input components
+ *
+ * Proptypes
+ * @param {string} defaultText is the placeholder text
+ * @param {string} storyId optional prop, used for comments
+ * @param {({storyId, value}) => void} onSubmit: (function) triggered when this post is submitted, takes {storyId, value} as parameters
+ */
+const NewPostInput = (props) => {
+  const [value, setValue] = useState("");
+
+  // called whenever the user types in the new post input box
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  // called when the user hits "Submit" for a new post
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.onSubmit && props.onSubmit(value);
+    setValue("");
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder={props.defaultText}
+        value={value}
+        onChange={handleChange}
+        className="NewPostInput-input"
+      />
+      <button
+        type="submit"
+        className="NewPostInput-button u-pointer"
+        value="Submit"
+        onClick={handleSubmit}
+      >
+        Submit
+      </button>
+    </div>
+  );
+};
+
+/**
+ * New Comment is a New Post component for comments
+ *
+ * Proptypes
+ * @param {string} defaultText is the placeholder text
+ * @param {string} storyId to add comment to
+ */
+const NewSocial = (props) => {
+  const addSocial = (value) => {
+    const body = { user: props.user, type: props.type, content: value };
+    post("/api/socialMedia", body).then((social) => {
+      // display this comment on the screen
+      props.addNewSocial(social);
+    });
+  };
+
+  return <NewPostInput defaultText={props.defaultText} onSubmit={addSocial} />;
+};
+
+const NewBubble = (props) => {
+  const updateStatus = (content) => {
+    const body = { content: content };
+    post("/api/status", body).then((status) => {
+      props.addNewBubble(bubble);
+    });
+  };
+
+  return <NewPostInput defaultText="Update Status" onSubmit={updateStatus} />;
+};
+
+const NewStatus = (props) => {
+  const addStatus = (value) => {
+    const body = { user: props.user, content: value, name: props.name };
+    post("/api/status", body).then((status) => {
+      props.addNewStatus(status);
+    });
+  };
+
+  return <NewPostInput defaultText={props.defaultText} onSubmit={addStatus} />;
+};
+
+// const NewAvatar = (props) => {
+//   const addAvatar = (value) => {
+//     const body = { user: props.user, content: value };
+//     post("/api/avatar", body).then((avatar) => {
+//       props.addNewAvatar(avatar);
+//     });
+//   };
+
+//   return <NewPostInput defaultText={props.defaultText} onSubmit={addAvatar} />;
+// };
+
+const NewAvatar = (props) => {
+  const [value, setValue] = useState("");
+
+  const addAvatar = (value) => {
+    const body = { user: props.user, content: value };
+    post("/api/avatar", body).then((avatar) => {
+      props.addNewAvatar(avatar);
+    });
+  };
+
+  // called when the user hits "Submit" for a new post
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addAvatar(value);
+    setValue("");
+  };
+
+  const encode = () => {
+    var selectedfile = document.getElementById("myinput").files;
+    if (selectedfile.length > 0) {
+      var imageFile = selectedfile[0];
+      var fileReader = new FileReader();
+      fileReader.onload = function (fileLoadedEvent) {
+        var srcData = fileLoadedEvent.target.result;
+        console.log(srcData);
+        setValue("ASDFG");
+        console.log(value);
+        var newImage = document.createElement("img");
+        newImage.src = srcData;
+        // setValue("HELLO");
+        // console.log("HELLO");
+        // console.log(value);
+        document.getElementById("dummy").innerHTML = newImage.outerHTML;
+        // console.log(document.getElementById("dummy").innerHTML);
+      };
+      fileReader.readAsDataURL(imageFile);
+    }
+  };
+
+  return (
+    <div>
+      <input id="myinput" type="file" onChange={encode} />
+      <button
+        type="submit"
+        className="NewPostInput-button u-pointer"
+        value="Submit"
+        onClick={handleSubmit}
+      >
+        Upload
+      </button>
+      <div id="dummy"></div>
+    </div>
+  );
+};
+
+/**
+ * New Story is a New Post component for comments
+ *
+ * Proptypes
+ * @param {string} defaultText is the placeholder text
+ */
+// const NewStory = (props) => {
+//   const addStory = (value) => {
+//     const body = { content: value };
+//     post("/api/story", body).then((story) => {
+//       // display this story on the screen
+//       props.addNewStory(story);
+//     });
+//   };
+
+//   return <NewPostInput defaultText="New Story" onSubmit={addStory} />;
+// };
+
+// /**
+//  * New Message is a New Message component for messages
+//  *
+//  * Proptypes
+//  * @param {UserObject} recipient is the intended recipient
+//  */
+// const NewMessage = (props) => {
+//   const sendMessage = (value) => {
+//     const body = { recipient: props.recipient, content: value };
+//     post("/api/message", body);
+//   };
+
+//   return <NewPostInput defaultText="New Message" onSubmit={sendMessage} />;
+// }
+
+export { NewSocial, NewBubble, NewStatus, NewAvatar };
---
+++ client/src/components/pages/EditWithAvatar.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from "react";
+import "./Edit.css";
+import "../../utilities.css";
+import { NewSocial, NewStatus, NewAvatar } from "../modules/NewPostInput.js";
+import SocialCard from "../modules/SocialCard.js";
+
+import { get } from "../../utilities";
+
+const Edit = (props) => {
+  const [status, setStatus] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [social, setSocial] = useState("");
+
+  const addStatus = (statusObj) => {
+    setStatus(statusObj);
+  };
+
+  const addAvatar = (avatarObj) => {
+    setAvatar(avatarObj);
+  };
+
+  const addSocial = (socialObj) => {
+    setSocial(socialObj);
+  };
+
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    get(`/api/user`, { userid: props.user }).then((userObj) => {
+      setUser(userObj);
+    });
+  }, []);
+
+  // const encode = () => {
+  //   var selectedfile = document.getElementById("myinput").files;
+  //   if (selectedfile.length > 0) {
+  //     var imageFile = selectedfile[0];
+  //     var fileReader = new FileReader();
+  //     fileReader.onload = function (fileLoadedEvent) {
+  //       var srcData = fileLoadedEvent.target.result;
+  //       var newImage = document.createElement("img");
+  //       newImage.src = srcData;
+  //       console.log(newImage.src);
+  //       document.getElementById("dummy").innerHTML = newImage.outerHTML;
+  //       // console.log(document.getElementById("dummy").innerHTML);
+  //     };
+  //     fileReader.readAsDataURL(imageFile);
+  //   }
+  // };
+
+  return (
+    <>
+      <div className="columnContainer">
+        <div className="columnItem box">
+          <h1 className="boxHeader"> socials </h1>
+          {props.user && <SocialCard type="linkedIn" user={props.user} />}
+          &nbsp;
+          {props.user && <SocialCard type="facebook" user={props.user} />}
+          &nbsp;
+          {props.user && <SocialCard type="instagram" user={props.user} />}
+          &nbsp;
+          {props.user && <SocialCard type="phoneNumber" user={props.user} />}
+        </div>
+        <div className="columnItem box">
+          <h1 className="boxHeader"> account info </h1>
+          {/* {props.user && (
+              <NewStatus addNewStatus={addStatus} defaultText="Your status" user={props.user} name = {user.name} />
+            )} */}
+          &nbsp;
+          {/* {props.user && (
+            <NewAvatar addNewAvatar={addAvatar} defaultText="Your avatar URL" user={props.user} />
+          )} */}
+          {props.user && <NewAvatar addNewAvatar={addAvatar} user={props.user} />}
+          &nbsp;
+          {/* <input id="myinput" type="file" onChange={encode} />
+          <div id="dummy"></div> */}
+        </div>
+      </div>
+      {/* <div className="wrapper">
+          <div className="columnItem">
+            <div className="rowContainer">
+              <div className="rowItem">
+                <div className="edit-Avatar">
+                  <img src="https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL2Zyb2ctMS5qcGciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjI5MH19fQ==" />
+                </div>
+              </div>
+              <div className="rowItem">
+                <button>DONE EDITING</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> */}
+    </>
+  );
+};
+
+export default Edit;
